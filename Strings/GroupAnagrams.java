fb5bc332805333f3f5ede1aff8221a788901d656
Piyush Arora <piyusharora1504@gmail.com>
2026-06-15 20:30:46 +0530

    Group all the anagrams together in an array of strings

--- Strings/GroupAnagrams.java
+++ Strings/GroupAnagrams.java
@@ -24,32 +24,63 @@ Output: [["a"]]
 
 Constraints:
 
-1 <= strs.length <= 104
+1 <= strs.length <= 10^4
 0 <= strs[i].length <= 100
 strs[i] consists of lowercase English letters.
 
+
+Approach: 
+
+1. First, we will create a hashmap to store the grouped anagrams. The key of the hashmap will be the sorted version of the string, and the value will be a list of strings that are anagrams of each other.
+
+2. We will iterate through each string in the input array, sort the characters of the string, and use the sorted string as the key to group the anagrams together in the hashmap.
+
+3. Finally, we will return the values of the hashmap as a list of lists, which will contain the grouped anagrams.
+
+
+Dry Run:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+- For "eat": sorted version is "aet". We add "eat" to the list corresponding to the key "aet".
+- For "tea": sorted version is "aet". We add "tea" to the list corresponding to the key "aet".
+- For "tan": sorted version is "ant". We add "tan" to the list corresponding to the key "ant".
+- For "ate": sorted version is "aet". We add "ate" to the list corresponding to the key "aet".
+- For "nat": sorted version is "ant". We add "nat" to the list corresponding to the key "ant".
+- For "bat": sorted version is "abt". We add "bat" to the list corresponding to the key "abt".
+
+Once we have processed all the strings, the hashmap will look like this:
+{
+  "aet": ["eat", "tea", "ate"],
+  "ant": ["tan", "nat"],
+  "abt": ["bat"]
+}
+
+Finally, we return the values of the hashmap as a list of lists:
+[["eat", "tea", "ate"], ["tan", "nat"], ["bat"]]
+
+
+Time Complexity: O(N * K log K) where N is the number of strings and K is the maximum length of a string (due to sorting each string).
+
+Space Complexity: O(N * K) for storing the grouped anagrams in the hashmap, where N is the number of strings and K is the maximum length of a string (since we are storing the sorted version of each string as a key in the hashmap).
+
+
 */
 
-// Approach : Sorting each string and using it as a key in a hashmap to group anagrams together.
-// We sort each string and store it in a hashmap where the key is the sorted string and the value is a list of anagrams.
 
 // Code:
+
 import java.util.*;
 class GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String,List<String>> mpp = new HashMap<>();
+        Map<String,List<String>> map = new HashMap<>();
         for(String s : strs){
-            char[] arr = s.toCharArray();
-            Arrays.sort(arr);
-            String str = new String(arr);
-            if(!mpp.containsKey(str)){
-                mpp.put(str,new ArrayList<>());
+            char[] arr = s.toCharArray(); // Convert the string to a character array
+            Arrays.sort(arr);  // Sort the character array to get the sorted version of the string
+            String str = new String(arr); // Convert the sorted character array back to a string
+            if(!map.containsKey(str)){
+                map.put(str,new ArrayList<>());
             }
-            mpp.get(str).add(s);
+            map.get(str).add(s);
         }
-        return new ArrayList<>(mpp.values());
+        return new ArrayList<>(map.values());
     }
 }
-
-// Time Complexity: O(N * K log K) where N is the number of strings and K is the maximum length of a string.
-// Space Complexity: O(N * K) for storing the grouped anagrams in the hashmap.
