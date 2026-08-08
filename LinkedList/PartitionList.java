38db2dd2e65366869b702fcf7ce65c4eb2a4abec
Piyush Arora <piyusharora1504@gmail.com>
2026-08-08 20:06:39 +0530

    Partition a Linked List based on a given value

--- LinkedList/PartitionList.java
+++ LinkedList/PartitionList.java
@@ -1,46 +1,74 @@
 // Problem Link: https://leetcode.com/problems/partition-list?envType=problem-list-v2&envId=linked-list
 
-// Approach:
-// 1. Create two dummy nodes to represent the heads of the two partitions: one for nodes less than x and one for nodes greater than or equal to x.
-// 2. Traverse the original linked list and append each node to the appropriate partition based on its value.
-// 3. After processing all nodes, connect the two partitions together.
-// 4. Ensure the last node of the greater than or equal to x partition points to null to avoid cycles.
-// 5. Return the head of the new partitioned list, which is the next node of the dummy node for the less than x partition.
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-}
-public class PartitionList {
+/*
+
+Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+Example 1:
+Input: head = [1,4,3,2,5,2], x = 3
+Output: [1,2,2,4,3,5]
+
+Example 2:
+Input: head = [2,1], x = 2
+Output: [1,2]
+
+Approach:
+1. First, we will create two dummy nodes, one for the "less than x" partition and another for the "greater than or equal to x" partition.
+2. We will iterate through the original linked list and for each node, we will check its value against x.
+3. If the node's value is less than x, we will append it to the "less than x" partition. Otherwise, we will append it to the "greater than or equal to x" partition.
+4. After processing all nodes, we will connect the two partitions together by linking the end of the "less than x" partition to the head of the "greater than or equal to x" partition.
+5. Finally, we will return the head of the "less than x" partition, which will be the new head of the modified linked list.
+
+
+Dry Run:
+Input: head = [1,4,3,2,5,2], x = 3
+1. Initialize two dummy nodes: lessHead and greaterHead.
+2. Initialize two pointers: less and greater, pointing to lessHead and greaterHead respectively.
+3. Start iterating through the original linked list:
+   - Node 1: value = 1 < 3, append to less partition.
+   - Node 4: value = 4 >= 3, append to greater partition.
+   - Node 3: value = 3 >= 3, append to greater partition.
+   - Node 2: value = 2 < 3, append to less partition.
+   - Node 5: value = 5 >= 3, append to greater partition.
+   - Node 2: value = 2 < 3, append to less partition.
+4. After processing all nodes, connect the two partitions:
+   - less.next = greaterHead.next (linking the end of less partition to the start of greater partition)
+5. Return lessHead.next as the new head of the modified linked list.
+
+Code:
+
+class PartitionList {
     public ListNode partition(ListNode head, int x) {
-        // Dummy nodes to represent the heads of the two partitions
-        ListNode lessHead = new ListNode(0);
+    
+        ListNode lessHead = new ListNode(0);        // Create two dummy nodes for the two partitions
         ListNode greaterHead = new ListNode(0);
         
-        // Pointers to the current end of the two partitions
-        ListNode less = lessHead;
+        ListNode less = lessHead;      // Pointers to build the two partitions
         ListNode greater = greaterHead;
         
-        // Traverse the original list and partition the nodes
-        while (head != null) {
-            if (head.val < x) {
-                less.next = head;  // Append to the 'less than x' partition
-                less = less.next;  // Move the pointer forward
-            } else {
-                greater.next = head;  // Append to the 'greater than or equal to x' partition
-                greater = greater.next;  // Move the pointer forward
+        while (head != null) {   // Iterate through the original linked list
+            if (head.val < x) {  // Append to the less partition
+                less.next = head;
+                less = less.next;
+            } else {    // Append to the greater partition
+                greater.next = head;
+                greater = greater.next;
             }
-            head = head.next;  // Move to the next node in the original list
-        }
+            head = head.next; // Move to the next node
+        }    // Connect the two partitions
+    
+        less.next = greaterHead.next; // Link the end of less partition to the start of greater partition
+        greater.next = null; // Terminate the greater partition
         
-        // Connect the two partitions
-        less.next = greaterHead.next;  // Link the end of 'less than x' to the start of 'greater than or equal to x'
-        greater.next = null;  // Ensure the last node points to null
-        
-        return lessHead.next;  // Return the head of the new partitioned list
+        return lessHead.next; // Return the head of the modified linked list
     }
 }
-// Time Complexity: O(n) - We traverse the list once.
-// Space Complexity: O(1) - We use a constant amount of extra space for the dummy nodes and pointers.
+
+
+Time Complexity: O(n), where n is the number of nodes in the linked list. We traverse the list once.
+
+Space Complexity: O(1), as we are using a constant amount of extra space for the dummy nodes and pointers.
+
+*/
